2571d616608222eeffada3a92da644ceca592d8e
SovietModernism <basileuski@gmail.com>
2020-06-26 12:37:55 +0300

    Добавлены комментарии и немного исправлен код.

--- SlavicComparator.py
+++ SlavicComparator.py
@@ -3,21 +3,21 @@ import json
 import re
 import os
 
-def getTranslation(wordURL, language2):
+def getTranslation(wordURL, language2):    # функция для получения перевода
     source = "https://iapi.glosbe.com/iapi3/wordlist?l1=ru&l2=" + language2 + "&q=" + wordURL + "&after=1&includeTranslations=true"
-    with urllib.request.urlopen(source) as url: data = json.loads(url.read().decode())
-    data = str(data)
-    parser = re.findall(r'\w+', data)
-    parser.remove('after')
+    with urllib.request.urlopen(source) as url: data = json.loads(url.read().decode())    # в словарь собирается вся информация из запроса
+    data = str(data)                      # полученный словарь превращается в строку для последующего получения слов
+    parser = re.findall(r'\w+', data)     # с помощью регулярных выражений отсеиваются слова
+    parser.remove('after')                # идёт удаление ненужных технических слов
     parser.remove('phrase')
     parser.remove('translations')
     parser.remove('success')
     parser.remove('True')
-    if (parser[0] != word):
+    if (parser[0] != word):               # проверка, действительно ли это перевод нужного слова, или же было просто переведено что-то похожее
         return "нет информации"
     else:
-        if (len(parser) >= 3):
-            if  parser[1] != parser[2].lower():
+        if (len(parser) >= 3):                           # если слова, следующие за нужным переводом в списке, не являются повторением этого самого
+            if  parser[1] != parser[2].lower():          # слова, то можно включить их в конечный результат, чтобы тот был точнее
                 return parser[1] + " / " + parser[2]
             elif (len(parser) >= 4) and (parser[1] != parser[3].lower()):
                 return parser[1] + " / " + parser[3]
@@ -25,19 +25,19 @@ def getTranslation(wordURL, language2):
                 return parser[1]
         else:
             return parser[1]
-
-def isFullyCyrillic(text):
+    
+def isFullyCyrillic(text):    # проверка на кириллическое слово (и наличие пробелов)
     try:
         for i in text:
             if bool(re.search('[\u0400-\u04FF]', i)) == False:
                 raise StopIteration
     except StopIteration:
-        return False
+        return False    # цикл был прерван = есть не-кириллическая буква
     else:
-        return True
+        return True     # цикл не был прерван = слово полностью на кириллице
 
 try:
-    if (urllib.request.urlopen("https://ru.glosbe.com").getcode() != 200):
+    if (urllib.request.urlopen("https://ru.glosbe.com").getcode() != 200):   # проверка на активность сайта
         print("Сайт Glosbe, на котором основано это приложение, сейчас по какой-то причине не работает!")
 except:
     print("Без интернета это приложение не сможет работать, а сейчас подключение отсутствует")
@@ -45,19 +45,19 @@ except:
 else:
     while (True):
         
-        while(True):
+        while(True):    # проверка слова на нормальность
             print("Введите слово на русском:")
             word = input()
-                
+            
             if len(word) <= 30:
                 if isFullyCyrillic(word) == True:
-                    wordURL = urllib.parse.quote(word)
+                    wordURL = urllib.parse.quote(word)    # перекодирование слова в URL-стиль для дальнейшей работы с ним
                     break
                 else:
                     print("В слове присутствуют не-кириллические символы либо пробелы, попробуйте ещё раз.")
             else:
                 print("Слово слишком длинное.")
-        
+                
         try:
             print("\nВОСТОЧНОСЛАВЯНСКИЕ ЯЗЫКИ\n")
                     
@@ -94,15 +94,18 @@ else:
                 break
             else:
                 os.system('cls')
-                
-        except ValueError:
+        
+        except ValueError:    # может возникнуть при работе функции getTranslation на этапе удаления ненужных слов, если тех вдруг не окажется
             print("Произошла ошибка при обработке нестандартных полученных данных.")
             os.system("pause")
+            break
         
-        except urllib.error.URLError:
+        except urllib.error.URLError:    # может возникнуть, когда urllib пытается работать с каким-либо сайтом, но интернета нет
             print("Во время выполнения программы пропало подключение к интернету")
             os.system("pause")
+            break
             
-        except:
+        except:    # ловит все остальные ошибки
             print("Произошла неизвестная ошибка, не связанная с интернетом или полученными данными.")
             os.system("pause")
+            break
