1e3ab04ae55683a3894a65f2f9fb527ae28e33b4
SovietModernism <basileuski@gmail.com>
2020-06-27 21:08:35 +0300

    Написана проверка подключения к сайту Glosbe, а также наличия интернета

--- SlavicComparatorGUI.py
+++ SlavicComparatorGUI.py
@@ -1,3 +1,5 @@
+import urllib.request
+import json
 import re
 from tkinter import *
 
@@ -44,28 +46,41 @@ window = Tk()
 window.title("Slavic Comparator")
 window.geometry('1000x500')
 
-# начальный текст
-entryText = Label(window, text = "Введите слово на русском:")
-entryText.grid(column = 0, row = 0, padx = 5, pady = 5, sticky = "w")
-entryText.config(font = ("Times New Roman", 13))
+# проверка на активность сайта, а также на наличие интернета
+try:
+    if (urllib.request.urlopen("https://ru.glosbe.com").getcode() != 200):
+        noSiteText = Label(window, text = "Сайт Glosbe, на котором основано это приложение, сейчас по какой-то причине не работает!")
+        noSiteText.grid(column = 0, row = 0, padx = 5, pady = 5, sticky = "w")
+        noSiteText.config(font = ("Times New Roman", 13), fg = "red")
 
-# строка с вводом
-entryWord = StringVar()    # переменная с текстом, введённым в виджет
-entryWidget = Entry(window, width = 40, textvariable = entryWord)
-entryWidget.grid(column = 1, row = 0, padx = 5, pady = 5)
-entryWidget.config(font = ("Times New Roman", 13))
+except urllib.error.URLError:
+    noConnectText = Label(window, text = "Без интернета это приложение не сможет работать, а сейчас подключение отсутствует.")
+    noConnectText.grid(column = 0, row = 0, padx = 5, pady = 5, sticky = "w")
+    noConnectText.config(font = ("Times New Roman", 13), fg = "red")
+    
+else:
+    # начальный текст
+    entryText = Label(window, text = "Введите слово на русском:")
+    entryText.grid(column = 0, row = 0, padx = 5, pady = 5, sticky = "w")
+    entryText.config(font = ("Times New Roman", 13))
 
-# кнопка для начала поиска
-startButton = Button(window, text = "Начать перевод", command = buttonClicked)
-startButton.grid(column = 2, row = 0, padx = 40, pady = 5)
-startButton.config(font = ("Times New Roman", 11))
+    # строка с вводом
+    entryWord = StringVar()    # переменная с текстом, введённым в виджет
+    entryWidget = Entry(window, width = 40, textvariable = entryWord)
+    entryWidget.grid(column = 1, row = 0, padx = 5, pady = 5)
+    entryWidget.config(font = ("Times New Roman", 13))
 
-# предупреждающий об ошибке текст, если введённое слово неверное
-warningText = Label(window, text = "В слове присутствуют не-кириллические символы либо пробелы!")
-warningText.config(font = ("Times New Roman", 12), fg = "red")
-warningText.grid_forget()
+    # кнопка для начала поиска
+    startButton = Button(window, text = "Начать перевод", command = buttonClicked)
+    startButton.grid(column = 2, row = 0, padx = 40, pady = 5)
+    startButton.config(font = ("Times New Roman", 11))
 
-# отслеживаем лимит
-entryWord.trace("w", lambda *args: character_limit(entryWord))
+    # предупреждающий об ошибке текст, если введённое слово неверное
+    warningText = Label(window, text = "В слове присутствуют не-кириллические символы либо пробелы!")
+    warningText.config(font = ("Times New Roman", 12), fg = "red")
+    warningText.grid_forget()
 
-window.mainloop
+    # отслеживаем лимит
+    entryWord.trace("w", lambda *args: character_limit(entryWord))
+
+    window.mainloop
