2b395658a3659cf18729b3ad6a0fb0c8c1a0f74e
SovietModernism <basileuski@gmail.com>
2020-06-27 20:56:40 +0300

    Рабочая строка для ввода слова, а также её проверки

--- SlavicComparatorGUI.py
+++ SlavicComparatorGUI.py
@@ -1,32 +1,43 @@
 import re
 from tkinter import *
 
+
+# переменная для проверки, была ли уже неудачная попытка ввести слово
 anErrorOnceOccured = None
 
-def character_limit(entry_word):
-    if len(entry_word.get()) > 30:
-        entry_word.set(entry_word.get()[:30])
+
+# лимит на 30 вводимых символов в строке
+def character_limit(entryWord):
+    if len(entryWord.get()) > 30:
+        entryWord.set(entryWord.get()[:30])
+
 
 # событие при клике на кнопку поиска
 def buttonClicked():
+    global anErrorOnceOccured
+    global warningText
     try:
-        for i in str(entry_word.get()):
+        
+        for i in str(entryWord.get()):
             if not bool(re.search('[\u0400-\u04FF]', i)):
                 raise StopIteration
                 
     except StopIteration:
-        warningText = Label(window, text = "В слове присутствуют не-кириллические символы либо пробелы!")
-        warningText.grid(column = 0, row = 2)
-        warningText.config(font = ("Times New Roman", 11), fg = "red")
-        anErrorOnceOccured = True
+            anErrorOnceOccured = True
+            warningText.grid(column = 0, row = 1, padx = 5, columnspan = 2, sticky = "w")
+            
     else:
+        if anErrorOnceOccured:
+            warningText.grid_forget()
         mainProgramPart()
 
 
 # основная часть программы
 def mainProgramPart():
-    if anErrorOnceOccured:
-        warningText.destroy()
+    pass
+
+
+
 
 # настройки окна
 window = Tk()
@@ -34,21 +45,27 @@ window.title("Slavic Comparator")
 window.geometry('1000x500')
 
 # начальный текст
-entryText = Label(window, text = "Введите слово на русском:", font = ("Times New Roman", 13))
-entryText.grid(column = 0, row = 0, padx = 5, pady = 5)
+entryText = Label(window, text = "Введите слово на русском:")
+entryText.grid(column = 0, row = 0, padx = 5, pady = 5, sticky = "w")
+entryText.config(font = ("Times New Roman", 13))
 
 # строка с вводом
-entry_word = StringVar()    # переменная с текстом, введённым в виджет
-entry_widget = Entry(window, width = 40, textvariable = entry_word)
-entry_widget.grid(column = 1, row = 0, pady = 5)
-entry_widget.config(font = ("Times New Roman", 13))
+entryWord = StringVar()    # переменная с текстом, введённым в виджет
+entryWidget = Entry(window, width = 40, textvariable = entryWord)
+entryWidget.grid(column = 1, row = 0, padx = 5, pady = 5)
+entryWidget.config(font = ("Times New Roman", 13))
 
 # кнопка для начала поиска
 startButton = Button(window, text = "Начать перевод", command = buttonClicked)
 startButton.grid(column = 2, row = 0, padx = 40, pady = 5)
 startButton.config(font = ("Times New Roman", 11))
 
-# лимит на 30 вводимых символов
-entry_word.trace("w", lambda *args: character_limit(entry_word))
+# предупреждающий об ошибке текст, если введённое слово неверное
+warningText = Label(window, text = "В слове присутствуют не-кириллические символы либо пробелы!")
+warningText.config(font = ("Times New Roman", 12), fg = "red")
+warningText.grid_forget()
+
+# отслеживаем лимит
+entryWord.trace("w", lambda *args: character_limit(entryWord))
 
 window.mainloop
