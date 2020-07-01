050ef5e4be52016972b358817689b3124cc26a2b
SovietModernism <basileuski@gmail.com>
2020-07-01 12:15:12 +0300

    Исправлено количество создаваемых label'ов; теперь каждому label'у при создании также присваивается текст

--- SlavicComparatorGUI.py
+++ SlavicComparatorGUI.py
@@ -34,31 +34,57 @@ def createLabels():
     
     global labelsList
     
-    L1 = Label(window)
-    L2 = Label(window)
-    L3 = Label(window)
-    L4 = Label(window)
-    L5 = Label(window)
-    L6 = Label(window)
-    L7 = Label(window)
-    L8 = Label(window)
-    L9 = Label(window)
-    L10 = Label(window)
-    L11 = Label(window)
-    L12 = Label(window)
-    L13 = Label(window)
-    L14 = Label(window)
-    L15 = Label(window)
-    L16 = Label(window)
-    L17 = Label(window)
-    L18 = Label(window)
-    L19 = Label(window)
-    L20 = Label(window)
-    L21 = Label(window)
+    # переменные для текста в label'ах
+    L1text = StringVar()
+    L1text.set(txt)
+    L2text = StringVar()
+    L3text = StringVar()
+    L4text = StringVar()
+    L5text = StringVar()
+    L6text = StringVar()
+    L7text = StringVar()
+    L8text = StringVar()
+    L9text = StringVar()
+    L10text = StringVar()
+    L11text = StringVar()
+    L12text = StringVar()
+    L13text = StringVar()
+    L14text = StringVar()
+    L15text = StringVar()
+    L16text = StringVar()
+    L17text = StringVar()
+    L18text = StringVar()
+    L19text = StringVar()
+    L20text = StringVar()
+    L21text = StringVar()
     
+    # создание самих label'ов
+    L1 = Label(window, textvariable = L1text)
+    L2 = Label(window, textvariable = L2text)
+    L3 = Label(window, textvariable = L3text)
+    L4 = Label(window, textvariable = L4text)
+    L5 = Label(window, textvariable = L5text)
+    L6 = Label(window, textvariable = L6text)
+    L7 = Label(window, textvariable = L7text)
+    L8 = Label(window, textvariable = L8text)
+    L9 = Label(window, textvariable = L9text)
+    L10 = Label(window, textvariable = L10text)
+    L11 = Label(window, textvariable = L11text)
+    L12 = Label(window, textvariable = L12text)
+    L13 = Label(window, textvariable = L13text)
+    L14 = Label(window, textvariable = L14text)
+    L15 = Label(window, textvariable =L15text)
+    L16 = Label(window, textvariable = L16text)
+    L17 = Label(window, textvariable = L17text)
+    L18 = Label(window, textvariable = L18text)
+    L19 = Label(window, textvariable = L19text)
+    L20 = Label(window, textvariable = L20text)
+    L21 = Label(window, textvariable = L21text)
+    
+    # помещение label'ов в список
     labelsList = [L1, L2, L3, L4, L5, L6, L7, L8,
-                         L9, L10, L11, L12, L13, L14, L15,
-                         L16, L17, L18, L19, L20, L21]
+                  L9, L10, L11, L12, L13, L14, L15,
+                  L16, L17, L18, L19, L20, L21]
     
 
 # основная часть программы
