79c54c1ccff70e23dc96e3638eda5ef9c15053c0
SovietModernism <basileuski@gmail.com>
2020-07-01 12:34:19 +0300

    Добавлены дополнительные label'ы-разграничители между языковыми группами; теперь label'ам при создании сразу указывается их местоположение, и они с самого начала размещаются на экране

--- SlavicComparatorGUI.py
+++ SlavicComparatorGUI.py
@@ -34,57 +34,41 @@ def createLabels():
     
     global labelsList
     
-    # переменные для текста в label'ах
-    L1text = StringVar()
-    L1text.set(txt)
-    L2text = StringVar()
-    L3text = StringVar()
-    L4text = StringVar()
-    L5text = StringVar()
-    L6text = StringVar()
-    L7text = StringVar()
-    L8text = StringVar()
-    L9text = StringVar()
-    L10text = StringVar()
-    L11text = StringVar()
-    L12text = StringVar()
-    L13text = StringVar()
-    L14text = StringVar()
-    L15text = StringVar()
-    L16text = StringVar()
-    L17text = StringVar()
-    L18text = StringVar()
-    L19text = StringVar()
-    L20text = StringVar()
-    L21text = StringVar()
+    # создание самих label'ов с текстом
+    L0 = Label(window, text = "ВОСТОЧНОСЛАВЯНСКИЕ ЯЗЫКИ")
+    L1 = Label(window, text = "Белорусский:")
+    L2 = Label(window, text = "Украинский:")
+    L3 = Label(window, text = "Русинский:")
+    L4 = Label(window, text = "Древнерусский:")
     
-    # создание самих label'ов
-    L1 = Label(window, textvariable = L1text)
-    L2 = Label(window, textvariable = L2text)
-    L3 = Label(window, textvariable = L3text)
-    L4 = Label(window, textvariable = L4text)
-    L5 = Label(window, textvariable = L5text)
-    L6 = Label(window, textvariable = L6text)
-    L7 = Label(window, textvariable = L7text)
-    L8 = Label(window, textvariable = L8text)
-    L9 = Label(window, textvariable = L9text)
-    L10 = Label(window, textvariable = L10text)
-    L11 = Label(window, textvariable = L11text)
-    L12 = Label(window, textvariable = L12text)
-    L13 = Label(window, textvariable = L13text)
-    L14 = Label(window, textvariable = L14text)
-    L15 = Label(window, textvariable =L15text)
-    L16 = Label(window, textvariable = L16text)
-    L17 = Label(window, textvariable = L17text)
-    L18 = Label(window, textvariable = L18text)
-    L19 = Label(window, textvariable = L19text)
-    L20 = Label(window, textvariable = L20text)
-    L21 = Label(window, textvariable = L21text)
+    L5 = Label(window, text = "\nЗАПАДНОСЛАВЯНСКИЕ ЯЗЫКИ")
+    L6 = Label(window, text = "Польский:")
+    L7 = Label(window, text = "Кашубский:")
+    L8 = Label(window, text = "Силезский:")
+    L9 = Label(window, text = "Верхнелужицкий:")
+    L10 = Label(window, text = "Нижнелужицкий:")
+    L11 = Label(window, text = "Полабский:")
+    L12 = Label(window, text = "Чешский:")
+    L13 = Label(window, text = "Словацкий:")
+    
+    L14 = Label(window, text = "\nЮЖНОСЛАВЯНСКИЕ ЯЗЫКИ")
+    L15 = Label(window, text = "Словенский:")
+    L16 = Label(window, text = "Хорватский:")
+    L17 = Label(window, text = "Сербский:")
+    L18 = Label(window, text = "Сербохорватский:")
+    L19 = Label(window, text = "Боснийский:")
+    L20 = Label(window, text = "Македонский:")
+    L21 = Label(window, text = "Болгарский:")
+    L22 = Label(window, text = "Церковнославянский:")
     
     # помещение label'ов в список
-    labelsList = [L1, L2, L3, L4, L5, L6, L7, L8,
+    labelsList = [L0, L1, L2, L3, L4, L5, L6, L7, L8,
                   L9, L10, L11, L12, L13, L14, L15,
-                  L16, L17, L18, L19, L20, L21]
+                  L16, L17, L18, L19, L20, L21, L22]
+    
+    # размещение label'ов в цикле
+    for i in range(0, 23):
+        labelsList[i].grid(column = 0, row = i+3)
     
 
 # основная часть программы
